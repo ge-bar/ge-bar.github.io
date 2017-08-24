8149c4498fa2e8b26ff2edfbcb1f54edf7a8b3be
Contact <contact@house2couette.com>
2017-08-25 00:51:49 +0200

    fonctionnel

--- script.js
+++ script.js
@@ -28,38 +28,173 @@ isRunningStandalone = function() {
 	return isRunningiOSStandalone || isRunningAndroidStandalone;
 };
 
-angular.module('bar', []).controller('TodoListController', function() {
-	var todoList = this;
-	todoList.todos = [ {
-		text : 'learn AngularJS',
-		done : true
-	}, {
-		text : 'build an AngularJS app',
-		done : false
-	} ];
+barModel = {
+	conso : {
+		kroDemi : {
+			prix : 3,
+			nb : 0
+		},
+		kroPinte : {
+			prix : 5,
+			nb : 0
+		},
+		kroPichet : {
+			prix : 14,
+			nb : 0
+		},
+		grimDemi : {
+			prix : 4,
+			nb : 0
+		},
+		grimPinte : {
+			prix : 7,
+			nb : 0
+		},
+		grimPichet : {
+			prix : 20,
+			nb : 0
+		},
+		kroSiropDemi : {
+			prix : 3.5,
+			nb : 0
+		},
+		kroSiropPinte : {
+			prix : 6,
+			nb : 0
+		},
+		kroSiropPichet : {
+			prix : 17,
+			nb : 0
+		},
+		kroMateDemi : {
+			prix : 3,
+			nb : 0
+		},
+		kroMatePinte : {
+			prix : 5,
+			nb : 0
+		},
+		vin12 : {
+			prix : 2.5,
+			nb : 0
+		},
+		vin24 : {
+			prix : 4.5,
+			nb : 0
+		},
+		vin48 : {
+			prix : 8,
+			nb : 0
+		},
+		vinPichet : {
+			prix : 24,
+			nb : 0
+		},
+		softDemi : {
+			prix : 1.5,
+			nb : 0
+		},
+		softPinte : {
+			prix : 2.5,
+			nb : 0
+		},
+		eau : {
+			prix : 1,
+			nb : 0
+		},
+		consigneGobelet : {
+			prix : 1,
+			nb : 0
+		},
+		consignePichet : {
+			prix : 2,
+			nb : 0
+		}
+	},
+	nbOptions : [0, 1, 2, 3, 4, 5, 6],
+	montant : 0,
+	monnaieRendu : 0,
+	monnaieDonnee : 0,
+	addNumber : function(number) {
+		var monnaieDonneeStr = this.monnaieDonnee.toString();
+		if (number == '.') {
+			if (monnaieDonneeStr.indexOf('.') == -1) {
+				this.monnaieDonnee += '' + number;
+			}
+		} else {
+			if (this.monnaieDonnee == 0) {
+				this.monnaieDonnee = number;
+			} else {
+				if (monnaieDonneeStr.indexOf('.') > -1) {
+					var numDecimal = monnaieDonneeStr.split('.')[1].length;
+					if (numDecimal < 2) {
+						this.monnaieDonnee += '' + number;
+					}
+				} else {
+					this.monnaieDonnee += '' + number;
+				}
+			}
+		}
+	},
+	resetMonnaie : function() {
+		this.monnaieDonnee = 0;
+	},
+	removeLast : function() {
+		var str = this.monnaieDonnee.toString();
+		var pos = str.length - 1;
+		if (pos == 0) {
+			this.monnaieDonnee = 0;
+		} else {
+			this.monnaieDonnee = str.substring(0, pos);
+		}
+	},
+	addConso : function(conso) {
+		this.conso[conso].nb += 1;
+	},
+	setConso : function(conso, nb) {
+		this.conso[conso].nb = nb;
+	},
+	calculMontant : function() {
+		var somme = 0;
+		for ( var conso in this.conso) {
+			somme += this.conso[conso].prix * this.conso[conso].nb;
+		}
+		this.montant = somme;
+	},
+	calculRendu : function() {
+		if (this.monnaieDonnee > 0 && this.montant > 0) {			
+			this.monnaieRendu = this.monnaieDonnee - this.montant;
+		}
+	}
+};
 
-	todoList.addTodo = function() {
-		todoList.todos.push({
-			text : todoList.todoText,
-			done : false
-		});
-		todoList.todoText = '';
+var app = angular.module('geBar', []);
+app.controller('geBarController', function($scope) {
+
+	$scope.bar = barModel;
+
+	$scope.numberButtonClicked = function(number) {
+		barModel.addNumber(number);
+		barModel.calculRendu();
 	};
 
-	todoList.remaining = function() {
-		var count = 0;
-		angular.forEach(todoList.todos, function(todo) {
-			count += todo.done ? 0 : 1;
-		});
-		return count;
-	};
+	$scope.clear = function() {
+		barModel.removeLast();
+		barModel.calculRendu();
+	}
 
-	todoList.archive = function() {
-		var oldTodos = todoList.todos;
-		todoList.todos = [];
-		angular.forEach(oldTodos, function(todo) {
-			if (!todo.done)
-				todoList.todos.push(todo);
-		});
-	};
+	$scope.clearAll = function() {
+		barModel.resetMonnaie();
+		barModel.calculRendu();
+	}
+
+	$scope.add = function(conso) {
+		barModel.addConso(conso);
+		barModel.calculMontant();
+		barModel.calculRendu();
+	}
+
+	$scope.set = function(conso, nb) {
+		barModel.setConso(conso, nb);
+	}
 });
